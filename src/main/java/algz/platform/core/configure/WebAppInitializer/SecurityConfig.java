aca509a2dcafc83573adc4d81d0c0c6784c88326
algz <algz1982@gmail.com>
2015-03-11 16:20:12 +0800

    .

--- src/main/java/algz/platform/core/configure/WebAppInitializer/SecurityConfig.java
+++ src/main/java/algz/platform/core/configure/WebAppInitializer/SecurityConfig.java
@@ -97,42 +97,42 @@ public class SecurityConfig {
 	 * @param secMan spring自动帮助注入参数，不用直接调用
 	 * @return
 	 */
-//	@Bean
-//	// 如果设置为原型模式，无循环依赖问题，但每次请求都将创建新对象。@Scope(value=ConfigurableBeanFactory.SCOPE_PROTOTYPE)
-//	public ShiroFilterFactoryBean shiroFilter(DefaultWebSecurityManager secMan){
-//		ShiroFilterFactoryBean filterFactory=new ShiroFilterFactoryBean();
-//		//Shiro的核心安全接口,这个属性是必须的 
-//		filterFactory.setSecurityManager(secMan);
-//		//要求登录时的链接(可根据项目的URL进行替换),非必须的属性,默认会自动寻找Web工程根目录下的"/login.jsp"页面
-//		filterFactory.setLoginUrl("/");
-//
-////        <!-- 登录成功后要跳转的连接(本例中此属性用不到,因为登录成功后的处理逻辑在LoginController里硬编码为main.jsp了) -->  
-////        <!-- <property name="successUrl" value="/system/main"/> -->  
-////        <!-- 用户访问未对其授权的资源时,所显示的连接 -->  
-////        <!-- 若想更明显的测试此属性可以修改它的值,如unauthor.jsp,然后用[玄玉]登录后访问/admin/listUser.jsp就看见浏览器会显示unauthor.jsp -->  
-//		filterFactory.setUnauthorizedUrl("/");
-////        <!-- Shiro连接约束配置,即过滤链的定义 -->  
-////        <!-- 此处可配合我的这篇文章来理解各个过滤连的作用http://blog.csdn.net/jadyer/article/details/12172839 -->  
-////        <!-- 下面value值的第一个'/'代表的路径是相对于HttpServletRequest.getContextPath()的值来的 -->  
-////        <!-- anon：它对应的过滤器里面是空的,什么都没做,这里.do和.jsp后面的*表示参数,比方说login.jsp?main这种 -->  
-////        <!-- authc：该过滤器下的页面必须验证后才能访问,它是Shiro内置的一个拦截器org.apache.shiro.web.filter.authc.FormAuthenticationFilter -->  
-////        <property name="filterChainDefinitions">  
-////            <value>  
-////                /mydemo/login=anon  
-////                /mydemo/getVerifyCodeImage=anon  
-////                /main**=authc  
-////                /user/info**=authc  
-////                /admin/listUser**=authc,perms[admin:manage]  
-////            </value>  
-////        </property>  
-//		Map<String,String> filterChainMap=new HashMap<String,String>();
-//		filterChainMap.put("/test/login", "anon");
-//		filterChainMap.put("/test/getVerifyCodeImage", "anon");
-//		filterChainMap.put("/test**", "authc");
-//		filterChainMap.put("/test/hello**", "authc");
-//		filterChainMap.put("/admin/listUser**", "authc,perms[admin:manage]");
-//		filterFactory.setFilterChainDefinitionMap(filterChainMap);
-//		return filterFactory;
-//	}
+	@Bean
+	// 如果设置为原型模式，无循环依赖问题，但每次请求都将创建新对象。@Scope(value=ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+	public ShiroFilterFactoryBean shiroFilter(DefaultWebSecurityManager secMan){
+		ShiroFilterFactoryBean filterFactory=new ShiroFilterFactoryBean();
+		//Shiro的核心安全接口,这个属性是必须的 
+		filterFactory.setSecurityManager(secMan);
+		//要求登录时的链接(可根据项目的URL进行替换),非必须的属性,默认会自动寻找Web工程根目录下的"/login.jsp"页面
+		filterFactory.setLoginUrl("/");
+
+//        <!-- 登录成功后要跳转的连接(本例中此属性用不到,因为登录成功后的处理逻辑在LoginController里硬编码为main.jsp了) -->  
+//        <!-- <property name="successUrl" value="/system/main"/> -->  
+//        <!-- 用户访问未对其授权的资源时,所显示的连接 -->  
+//        <!-- 若想更明显的测试此属性可以修改它的值,如unauthor.jsp,然后用[玄玉]登录后访问/admin/listUser.jsp就看见浏览器会显示unauthor.jsp -->  
+		filterFactory.setUnauthorizedUrl("/");
+//        <!-- Shiro连接约束配置,即过滤链的定义 -->  
+//        <!-- 此处可配合我的这篇文章来理解各个过滤连的作用http://blog.csdn.net/jadyer/article/details/12172839 -->  
+//        <!-- 下面value值的第一个'/'代表的路径是相对于HttpServletRequest.getContextPath()的值来的 -->  
+//        <!-- anon：它对应的过滤器里面是空的,什么都没做,这里.do和.jsp后面的*表示参数,比方说login.jsp?main这种 -->  
+//        <!-- authc：该过滤器下的页面必须验证后才能访问,它是Shiro内置的一个拦截器org.apache.shiro.web.filter.authc.FormAuthenticationFilter -->  
+//        <property name="filterChainDefinitions">  
+//            <value>  
+//                /mydemo/login=anon  
+//                /mydemo/getVerifyCodeImage=anon  
+//                /main**=authc  
+//                /user/info**=authc  
+//                /admin/listUser**=authc,perms[admin:manage]  
+//            </value>  
+//        </property>  
+		Map<String,String> filterChainMap=new HashMap<String,String>();
+		filterChainMap.put("/test/login", "anon");
+		filterChainMap.put("/test/getVerifyCodeImage", "anon");
+		filterChainMap.put("/test**", "authc");
+		filterChainMap.put("/test/hello**", "authc");
+		filterChainMap.put("/admin/listUser**", "authc,perms[admin:manage]");
+		filterFactory.setFilterChainDefinitionMap(filterChainMap);
+		return filterFactory;
+	}
 	
 }
--- src/main/java/algz/platform/core/exception/ALGZExceptionHandler.java
+++ src/main/java/algz/platform/core/exception/ALGZExceptionHandler.java
@@ -10,8 +10,19 @@ public class ALGZExceptionHandler implements HandlerExceptionResolver {
 
 	@Override
 	public ModelAndView resolveException(HttpServletRequest arg0,
-			HttpServletResponse arg1, Object arg2, Exception arg3) {
-		// TODO Auto-generated method stub
+			HttpServletResponse arg1, Object handler, Exception ex) {
+
+//        Map<String, Object> model = new HashMap<String, Object>();  
+//        model.put("ex", ex);  
+//        // 根据不同错误转向不同页面  
+//        if(ex instanceof BusinessException) {  
+//            return new ModelAndView("business_error", model);  
+//        }else if(ex instanceof ParameterException) {  
+//            return new ModelAndView("parameter_error", model);  
+//        } else {  
+//            return new ModelAndView("error", model);  
+//        } 
+		
 		return null;
 	}
 
